7d47926e7278596b7cb48599ec054f966f9ef9fd
Jordana Cavalcante <88212453+jojoDev02@users.noreply.github.com>
2023-11-16 10:34:14 -0300

    Delete src/config.py

--- src/config.py
+++
@@ -1,5 +0,0 @@
-MAIL_SERVER = 'smtp.mailgun.org'
-MAIL_PORT = 587 
-MAIL_USE_SSL = True
-MAIL_USERNAME = 'postmaster@sandbox1a51404520ad47b39c3619f60e22f48b.mailgun.org'  # Substitua pelo seu e-mail do Gmail
-MAIL_PASSWORD = 'f7840c01758091c7b78d226aa344bba9-1c7e8847-45c47456'  # Substitua pela senha do seu e-mail do Gmail
